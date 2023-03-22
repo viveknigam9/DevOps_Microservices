9173c852f97163372bda8f55c732c4af51f80f80
viveknigam9 <nigam.vivek4@gmail.com>
2023-03-22 16:04:02 +0000

    Kubernetes update

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="viveknig9/repo001/"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityproj  --image=$dockerpath  --port=80 --labels app=udacityproj
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityproj 8000:80
